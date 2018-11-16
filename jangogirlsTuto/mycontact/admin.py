cf25e43d91f85719512da02048e41f12c588b4a9
Jang Woojin <ehdwlsdnwls@naver.com>
2018-11-16 16:43:23 +0900

    make jangogirls tuto

---
+++ jangogirlsTuto/mycontact/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Contact, MyContact
+
+admin.site.register(Contact)
+admin.site.register(MyContact)
---
+++ jangogirlsTuto/mycontact/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Contact(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+        from django.db import models
+from django.utils import timezone
+
+
+class MyContact(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    e_mail_address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    detail = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
